6a5805490ae5e11875efbb8c3c0b4adb55303736
suin lee <silee@weedscomm.com>
2021-11-01 10:31:32 +0900

    random Dice

--- developing-android-app-with-kotlin/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ developing-android-app-with-kotlin/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,19 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        resultText.text = randomInt.toString()
 
     }
 }
