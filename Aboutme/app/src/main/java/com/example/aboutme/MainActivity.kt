a9bed9a4dad2d26daa62b1cd916e9b66f7ad1be2
suin lee <silee@weedscomm.com>
2021-11-04 23:27:20 +0900

    hide the keyboard

--- Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.aboutme
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            showDescription(it)
+        }
+
+    }
+
+    private fun showDescription(view: View) {
+        val description = findViewById<TextView>(R.id.description)
+        val editText = findViewById<TextView>(R.id.edit_text)
+
+        editText.visibility = View.GONE
+        description.visibility = View.VISIBLE
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+
     }
 }
