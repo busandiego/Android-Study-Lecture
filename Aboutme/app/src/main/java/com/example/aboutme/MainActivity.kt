9ed2ca037b02b68777d05f60ddd515125e8ca3be
suin lee <silee@weedscomm.com>
2021-11-05 00:02:29 +0900

    add androidx and set data binding

--- Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,36 +2,47 @@ package com.example.aboutme
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             showDescription(it)
         }
 
     }
 
     private fun showDescription(view: View) {
-        val description = findViewById<TextView>(R.id.description)
-        val editText = findViewById<TextView>(R.id.edit_text)
 
-        editText.visibility = View.GONE
-        description.visibility = View.VISIBLE
+       /* binding.description.visibility = View.VISIBLE
+        binding.editText.visibility = View.GONE*/
+
+        binding.apply {
+            invalidateAll()
+            description.visibility = View.VISIBLE
+            editText.visibility = View.GONE
+        }
+
 
         // Hide the keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
-
 
     }
 }
