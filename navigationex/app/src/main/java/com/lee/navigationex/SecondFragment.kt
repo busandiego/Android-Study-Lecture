8c654938ff1963bc039fa9731d0002a82936fcb8
suin lee <silee@weedscomm.com>
2021-11-14 18:14:55 +0900

    fragment change ex

--- navigationex/app/src/main/java/com/lee/navigationex/SecondFragment.kt
+++ navigationex/app/src/main/java/com/lee/navigationex/SecondFragment.kt
@@ -9,27 +9,25 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.lee.navigationex.databinding.FragmentFirstBinding
+import com.lee.navigationex.databinding.FragmentSecondBinding
 
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
+ * Use the [SecondFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentFirstBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_first, container, false
+        val binding: FragmentSecondBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_second, container, false
         )
 
-        binding.playButton.setOnClickListener {
-            view: View -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
 
         return binding.root
 
