d3e46af3ddc21c7b9bc14fa4fb71b685da10760e
suin lee <silee@weedscomm.com>
2021-11-21 22:19:29 +0900

    fragment add action

--- navigationex/app/src/main/java/com/lee/navigationex/TitleFragment.kt
+++ navigationex/app/src/main/java/com/lee/navigationex/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.lee.navigationex.databinding.FragmentTitleBinding
 
@@ -17,11 +18,17 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container,false)
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container,false)
 
-        binding.playButton.setOnClickListener {
-            view: View -> view.findNavController(view).navigate(R.id.action.action_titleFragment_to_gameFragment)
+        binding.playButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            // view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment) 이거 가능
         }
+
+        /*binding.playButton.setOnClickListener {
+            view: View -> view.findNavController().navigate(R.id.action.action_titleFragment_to_gameFragment)
+        }*/
 
 
         return binding.root
