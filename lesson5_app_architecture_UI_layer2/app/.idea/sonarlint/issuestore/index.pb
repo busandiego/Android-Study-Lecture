b6d303784596e23f8d77a55e259ed35061bdadb8
suin lee <silee@weedscomm.com>
2022-03-28 22:28:15 +0900

    copy projects

---
+++ lesson5_app_architecture_UI_layer2/app/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+{
+Ksrc/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt,4\f\4f9282a1b9fdfff3d14b43d360e1c37220fd6a64
+|
+Lsrc/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt,c\b\cb5d886d8d1f5dcb791cec7629c88e18a63a788a
+U
+%src/main/res/layout/game_fragment.xml,1\5\15bf645f061ba6559345dff9290e4f1dbf78edc8
---
+++ lesson5_app_architecture_UI_layer2/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,182 @@
+package com.example.android.guesstheword.screens.game
+
+import android.database.DatabaseUtils
+import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import androidx.lifecycle.Transformations
+
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
+class GameViewModel: ViewModel() {
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
+    }
+
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+
+        // This is the total time of the game
+        // const val COUNTDOWN_TIME = 60000L
+        const val COUNTDOWN_TIME = 10000L
+
+        // This is the time when the phone will start buzzing each second
+        private const val COUNTDOWN_PANIC_SECONDS = 10L
+    }
+
+    private val timer: CountDownTimer
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    // var score = 0
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    // LiveData로만 XML로 바로 접근 가능.
+    val currentTimeString: LiveData<String>
+    = Transformations.map(currentTime, { time ->
+        DateUtils.formatElapsedTime(time)
+    })
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
+    // Event that triggers the phone to buzz using different patterens, determined by BuzzType
+    private val _eventBuzz = MutableLiveData<BuzzType>()
+    val eventBuzz: LiveData<BuzzType>
+        get() = _eventBuzz
+
+
+    init {
+        Log.i("GameViewModel", "GameViewModel init")
+        resetList()
+        nextWord()
+        _score.value = 0
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("GameViewModel", "millisUntilFinished >>>> $millisUntilFinished")
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+
+                // count down시 buzz
+                if (millisUntilFinished / ONE_SECOND <= COUNTDOWN_PANIC_SECONDS) {
+                    _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
+                }
+
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventBuzz.value = BuzzType.GAME_OVER
+                _eventGameFinish.value = true
+            }
+        }
+
+        timer.start()
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+        Log.i("GameVioewModel", "GameVideModel destroyed!")
+    }
+
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //  _eventGameFinish.value = true
+            resetList()
+        }
+        _word.value = wordList.removeAt(0)
+
+        // updateWordText()
+        // updateScoreText()
+    }
+
+
+    /** Methods for buttons presses **/
+    fun onSkip() {
+        _score.value = (_score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (_score.value)?.plus(1)
+        _eventBuzz.value = BuzzType.CORRECT
+        nextWord()
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+
+    }
+
+    fun onBuzzComplete() {
+        _eventBuzz.value = BuzzType.NO_BUZZ
+    }
+
+}
